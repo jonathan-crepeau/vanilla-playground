6b92e8fc090a8de8b5f84b435b07dc62b972036c
jonathan-crepeau <jon.crepeau@gmail.com>
2022-07-01 16:39:46 -0700

    Part 1 iterable/enumarable practice complete

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('Fear is the mind killer.');
+// console.log('Fear is the mind killer.');
+let log = console.log; 
+
+const person1 = {
+  name: 'Jonathan',
+  age: 34,
+  location: "San Francisco, CA, USA",
+  hairColor: "brown",
+  beardColor: "red",
+};
+
+const names = [
+  "Gandalf",
+  "Bilbo",
+  "Aragorn",
+  "Gimli",
+]
+
+names[4] = "Legolas";
+Object.defineProperty(names, "1", {enumerable: false});
+
+for (let n in names) {
+  log(n);
+}
+log();
+
+
+
+
