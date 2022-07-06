f1f1ba194f255a9c09910154bcd59626ea659759
jonathan-crepeau <jon.crepeau@gmail.com>
2022-07-06 16:29:58 -0700

    iterable and enumerable exercises compelte

--- js/app.js
+++ js/app.js
@@ -22,7 +22,9 @@ const names = [
 
 // NOTE - Example of dot notation used to define a new property directly on the Object.
 // By default, values added usig dot notation are enumerable.
-names.elf = "Legolas";
+names[4] = "Legolas";
+// names.push('Legolas')
+// Object.defineProperty(names, '4', {value: 'Legolas'});
 
 // NOTE - The static method Object.defineProperty() defines a new property directly on the Object (in this case the names array). It can also be used as a method to modify an existing property on an object.
 // By default, values added using Object.defineProperty() are immutable and NOT enumerable.
@@ -49,4 +51,42 @@ Object.defineProperty(middleEarth, 'age', {value: '3', enumerable: true});
 
 // for (let p in middleEarth) {
 //   console.log(p + ' - ' + middleEarth[p]);
+// }
+
+
+
+// ====================
+// SECTION - Example 1, for/of loops
+// ====================
+
+// NOTE - Using the names array from for/in examples above:
+// for (let x of names){
+//   console.log(x)
+// }
+
+// NOTE - const will not work (vs. let keyword above) because we are reassigning the 'b; variable inside the block:
+// for (const b of names) {
+//   b = 'nope';
+//   console.log(b);
+// }
+
+// ======================================================
+// REVIEW - Thesis:
+// The for/of loop above used with the 'names' array will NOT unless the property has a numeric key.
+
+// ANSWER:
+// https://stackoverflow.com/questions/52395924/how-can-javascript-arrays-have-non-numeric-keys
+  // non-numeric properties are usually skipped by the iterator (for arrays). Arrays, generally, do not have arbitrary non-numeric properties. Any code that DOES assign arbitrary non-numeric properties to an array is likely in need of refactor ... OR, by example. Think of Steve's examples of for/in vs. for/of I watched on youtube.
+
+// ====================
+// SECTION - Example 2, for/of loops
+// ====================
+
+// let array1 = ['Meredith', 'Christina', 'Izzie', 'George'];
+
+// // If defined with a numeric index, the following property will be returned with the use of a for/of loop:
+// Object.defineProperty(array1, '4', { value: 'Alex'})
+
+// for (const b of array1){
+//   console.log(b);
 // }
